fcc027498f218289747538e6bef4520d53c3ea19
ahmed50-50s <ak728233@gmail.com>
2025-03-26 02:52:41 +0200

    solve extention from JSX to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import API from './components/API/API.JSX'
+import API from './components/API/API.Jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
